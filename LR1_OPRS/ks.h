a8c045287c9f629e0f20f58a08d71491f83ada97
DESKTOP-R2B8F43\Валерия <mrs.valerya@yandex.ru>
2022-11-09 17:15:44 +0300

    +new ks

--- LR1_OPRS/ks.h
+++ LR1_OPRS/ks.h
@@ -6,7 +6,7 @@
 #include "statisticalCharacteristics.h"
 using namespace std;
 
-void update_max(double& max, double& curr)
+void calc_max(double& max, double& curr)
 {
 	if (curr >= max)
 	{
@@ -23,7 +23,7 @@ vector <double> sort_(vector <double> data) { // Сортировка векто
 		//cout << "i= " << tmp[i] << endl;
 	}
 
-	double temp = -10000.0;
+	double temp = -1000000.0;
 	for (int i = 0; i < (n - 1); i++)
 	{
 		if (tmp[i] >= tmp[i + 1]) {
@@ -38,14 +38,15 @@ vector <double> sort_(vector <double> data) { // Сортировка векто
 
 double KS(vector<double> data_) { // Вычисление значения статистики Колмогорова-Смирнова
 	double max = 0;// max supremum
-	double curr = 0;// current supremum
 	double count_low = 0;
 	double count_high = 0;
 	double sum_low = 0;
 	double sum_high = 0;
+
 	int n = data_.size();
 	double mean_ = mean(data_);
 	double sigma = sqrt(disp(data_, mean_));
+
 	vector <double> data(n);
 	data = sort_(data_);
 
@@ -56,16 +57,16 @@ double KS(vector<double> data_) { // Вычисление значения ст�
 			count_high++;
 			sum_low = abs(0.5 * (1 + erf((data[i] - mean_) / (sqrt(2) * sigma))) - (count_low / n));
 			sum_high = abs((count_high / n) - 0.5 * (1 + erf((data[i] - mean_) / (sqrt(2) * sigma))));
-			update_max(sum_high, sum_low);// Максимум сравнения записываем в sum_high(текущий Sup)
-			update_max(max, sum_high);
+			calc_max(sum_high, sum_low);// Максимум сравнения записываем в sum_high(текущий Sup)
+			calc_max(max, sum_high);
 			count_low = count_high;
 			if (i == (n - 2))
 			{
 				count_high++;
 				sum_low = abs(0.5 * (1 + erf((data[i] - mean_) / (sqrt(2) * sigma))) - (count_low / n));
 				sum_high = abs((count_high / n) - 0.5 * (1 + erf((data[i] - mean_) / (sqrt(2) * sigma))));
-				update_max(sum_high, sum_low);// Максимум сравнения записываем в sum_high
-				update_max(max, sum_high);
+				calc_max(sum_high, sum_low);// Максимум сравнения записываем в sum_high
+				calc_max(max, sum_high);
 				count_low = count_high;
 			}
 		}
@@ -77,8 +78,8 @@ double KS(vector<double> data_) { // Вычисление значения ст�
 				count_high++;
 				sum_low = abs(0.5 * (1 + erf((data[i] - mean_) / (sqrt(2) * sigma))) - (count_low / n));
 				sum_high = abs((count_high / n) - 0.5 * (1 + erf((data[i] - mean_) / (sqrt(2) * sigma))));
-				update_max(sum_high, sum_low);// максимум сравнения записываем в sum_high
-				update_max(max, sum_high);
+				calc_max(sum_high, sum_low);// максимум сравнения записываем в sum_high
+				calc_max(max, sum_high);
 				count_low = count_high;
 			}
 		}
